7787a68e694ea118afda1ce87d9ddd2c02b8f53c
Tristan Roux <tr.tristan-roux@outlook.fr>
2019-02-28 21:53:00 +0100

    Working on the library display.

--- js/Library.js
+++ js/Library.js
@@ -1,26 +1,40 @@
 class Game
 {
     name;
+    id;
     thumbnail;
+    launcher;
+    isInstalled;
+    localPath;
 
     constructor(name, thumbnail)
     {
         this.name = name;
         this.thumbnail = thumbnail;
     }
+
+    // constructor(name, id, thumbnail, launcher, isInstalled, localPath)
+    // {
+    //     this.name = name;
+    //     this.id = id
+    //     this.thumbnail = thumbnail;
+    //     this.launcher = launcher;
+    //     this.isInstaleld = isInstalled;
+    //     this.localPath = localPath;
+    // }
 }
 
 
 function populateGrid()
 {
-    console.log("test");
-    var games = ["La ligue du sel", "Overnetoyé" ];
+    var games = [new Game("La ligue du sel", "https://news-a.akamaihd.net/public/images/misc/GameBox.jpg"), new Game("Overnetoyé", "https://images-na.ssl-images-amazon.com/images/I/51kkc%2BjHrGL.jpg") ];
 
     let grid = document.getElementById("library");
     for(var i = 0; i < games.length; i++)
     {
-        let gridHtml = "<div>" + games[i] + "</div>";
+        let gridHtml = "<img src='" + games[i].thumbnail + "'/> <p>" + games[i].name + "</p>";
         let element = document.createElement("div");
+        element.className = "game";
         element.innerHTML = gridHtml;
         grid.appendChild(element);
     }
