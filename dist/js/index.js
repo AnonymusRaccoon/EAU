57b25f4ebac220ba965541f644a669957264fab0
Tristan Roux <tr.tristan-roux@outlook.fr>
2019-03-20 22:02:01 +0100

    Adding oidc initialisation.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,16 @@
 "use strict";
-function openSettings() {
-    $("#content").load("dist/html/settings.html");
-}
 function home() {
     $("#content").load("dist/html/home.html", () => {
         setup();
         populateGrid();
     });
+    document.getElementById("title").innerHTML = "EAU";
+}
+function openSettings() {
+    $("#content").load("dist/html/settings.html");
+    document.getElementById("title").innerHTML = "<i class='icon fas fa-arrow-left'></i>  Settings";
+}
+function steamLogIn() {
+    console.log("Logging into steam");
+    new Oidc.UserManager("https://steamcommunity.com/openid", "7C218E8D1347C3CD6CB8117E5ED533BC", "localhost:5500");
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,15 +1,24 @@
-function openSettings()
-{
-    $("#content").load("dist/html/settings.html");
-}
-
 function home()
 {
     $("#content").load("dist/html/home.html", () =>
     {
         setup()
         populateGrid();
-    });   
+    });
+    document.getElementById("title").innerHTML = "EAU";
 }
 
+function openSettings()
+{
+    $("#content").load("dist/html/settings.html");
+    document.getElementById("title").innerHTML = "<i class='icon fas fa-arrow-left'></i>  Settings";
+}
 
+//Log In
+declare var Oidc: UserManager;
+
+function steamLogIn()
+{
+    console.log("Logging into steam");
+    new Oidc.UserManager("https://steamcommunity.com/openid", "7C218E8D1347C3CD6CB8117E5ED533BC", "localhost:5500");
+}
